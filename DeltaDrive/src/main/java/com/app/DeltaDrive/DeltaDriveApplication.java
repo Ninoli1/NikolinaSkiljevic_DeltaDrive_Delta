99aa3dcd15aaffc3a4756814786cf7e726588548
Ninoli1 <nikolinaskiljevic@gmail.com>
2024-10-24 15:42:37 +0200

    Inital Setup

---
+++ DeltaDrive/src/main/java/com/app/DeltaDrive/DeltaDriveApplication.java
@@ -0,0 +1,13 @@
+package com.app.DeltaDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaDriveApplication.class, args);
+	}
+
+}
