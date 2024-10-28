e3bd77030a867813993065b39db73c8d550185bc
Ninoli1 <nikolinaskiljevic@gmail.com>
2024-10-28 22:18:34 +0100

    build: Database and OpenAPI improved

--- DeltaDrive/src/main/java/com/app/DeltaDrive/DeltaDriveApplication.java
+++ DeltaDrive/src/main/java/com/app/DeltaDrive/DeltaDriveApplication.java
@@ -1,13 +1,18 @@
 package com.app.DeltaDrive;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(basePackages = "com.app.DeltaDrive.model")
 @SpringBootApplication
 @ComponentScan
+@EnableJpaRepositories(basePackages = "com.app.DeltaDrive.repository")
+@OpenAPIDefinition(security = { @SecurityRequirement(name = "jwt"), @SecurityRequirement(name = "basic") })
 public class DeltaDriveApplication {
 
 	public static void main(String[] args) {
